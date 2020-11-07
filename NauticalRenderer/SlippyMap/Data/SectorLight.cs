871f74198bd0b84ac2b69c1ed0eb293e167ca201
myluki2000 <lukas636@live.com>
2020-11-07 01:57:16 +0100

    Sector light refactoring

---
+++ NauticalRenderer/SlippyMap/Data/SectorLight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace NauticalRenderer.SlippyMap.Data
+{
+    public struct SectorLight
+    {
+        public Vector2 Coordinates;
+        public bool Major;
+        public Sector[] Sectors;
+
+        public struct Sector
+        {
+            public float StartAngle;
+            public float EndAngle;
+            public float Orientation;
+            public float Range;
+            public Color Color;
+        }
+    }
+}
--- NauticalRenderer/SlippyMap/Layers/LandmarkLayer.cs
+++ NauticalRenderer/SlippyMap/Layers/LandmarkLayer.cs
@@ -26,6 +26,8 @@ namespace NauticalRenderer.SlippyMap.Layers
         private Landmark[] landmarks;
         private List<ICompleteOsmGeo> lights;
 
+        private SectorLight[] sectorLights;
+
         /// <inheritdoc />
         public override void LoadContent(MapPack mapPack)
         {
@@ -61,6 +63,76 @@ namespace NauticalRenderer.SlippyMap.Layers
                     return new Landmark(Landmark.LandmarkCategory.GENERIC, OsmHelpers.GetCoordinateOfOsmGeo(x));
                 })
                 .ToArray();
+
+            sectorLights = lights.Where(x => !x.Tags.ContainsKey("seamark:light:colour")).Select(x =>
+            {
+                SectorLight sl = new SectorLight();
+
+                sl.Major = !x.Tags.Contains("seamark:type", "light_minor");
+                sl.Coordinates = OsmHelpers.GetCoordinateOfOsmGeo(x);
+
+                List<SectorLight.Sector> sectors = new List<SectorLight.Sector>();
+                int i = 1;
+                while (true)
+                {
+                    SectorLight.Sector s = new SectorLight.Sector();
+                    s.Orientation = float.NaN;
+                    s.Range = 30;
+
+                    Dictionary<string, Tag> tags = x.Tags.Where(x => x.Key.StartsWith("seamark:light:" + i + ":")).ToDictionary(x => x.Key.Replace("seamark:light:" + i + ":", ""));
+                    i++;
+
+                    // we have reached the end!
+                    if (tags.Count == 0) break;
+                    // we can't draw the light if we don't know the color
+                    if (!tags.ContainsKey("colour")) continue;
+
+                    if (tags.TryGetValue("range", out Tag rangeTag))
+                        s.Range = float.Parse(rangeTag.Value, CultureInfo.InvariantCulture) * (s.Range / 10);
+
+                    if (tags.ContainsKey("sector_start") && tags.ContainsKey("sector_end") && tags["sector_start"].Value != tags["sector_end"].Value)
+                    {
+                        s.StartAngle = Utility.Utility.NormalizeAngleRad(
+                            MathHelper.ToRadians(float.Parse(tags["sector_start"].Value, CultureInfo.InvariantCulture)) - MathHelper.Pi);
+                        s.EndAngle = Utility.Utility.NormalizeAngleRad(
+                            MathHelper.ToRadians(float.Parse(tags["sector_end"].Value, CultureInfo.InvariantCulture)) - MathHelper.Pi);
+                    }
+                    else
+                    {
+                        // check if light has orientation instead
+                        if (!tags.ContainsKey("orientation"))
+                        {
+                            // if not that means that sector_start and end are the same value so use these as orientation instead
+                            if (tags.ContainsKey("sector_start") && tags.ContainsKey("sector_end"))
+                            {
+                                s.Orientation = float.Parse(tags["sector_start"].Value, CultureInfo.InvariantCulture);
+                            }
+                            else
+                            {
+                                continue;
+                            }
+
+                        }
+                        else
+                        {
+                            s.Orientation = MathHelper.ToRadians(float.Parse(tags["orientation"].Value, CultureInfo.InvariantCulture)) - MathHelper.Pi;
+                        }
+
+
+                        s.StartAngle = Utility.Utility.NormalizeAngleRad(s.Orientation - 0.05f);
+                        s.EndAngle = Utility.Utility.NormalizeAngleRad(s.Orientation + 0.05f);
+                    }
+
+                    s.Color = OsmHelpers.GetColorFromSeamarkColor(tags["colour"].Value);
+                    if (s.Color == Color.White) s.Color = Color.Yellow;
+
+                    sectors.Add(s);
+                }
+
+                sl.Sectors = sectors.ToArray();
+
+                return sl;
+            }).ToArray();
         }
 
         /// <inheritdoc />
@@ -157,6 +229,8 @@ namespace NauticalRenderer.SlippyMap.Layers
                         DrawName(sb, o, camera);
                 }
             }
+
+            DrawSectorLights(sb, camera);
         }
 
         private void DrawName(SpriteBatch sb, ICompleteOsmGeo o, Camera camera)
@@ -184,22 +258,6 @@ namespace NauticalRenderer.SlippyMap.Layers
                 color = OsmHelpers.GetColorFromSeamarkColor(lightColor);
                 if(color == Color.White) color = Color.Yellow;
             }
-            else
-            {
-                List<Tag> colourKeyValuePairs = o.Tags.Where(x => x.Key.StartsWith("seamark:light:") && x.Key.EndsWith(":colour")).ToList();
-                if (colourKeyValuePairs.Count > 0)
-                {
-                    if (colourKeyValuePairs.Count == 1) color = OsmHelpers.GetColorFromSeamarkColor(colourKeyValuePairs[0].Value);
-                    if (color == Color.White) color = Color.Yellow;
-
-                    // draw sector lights
-                    if (drawSectorLights) DrawSectorLights(sb, o, camera);
-                }
-                else
-                {
-                    return; // object doesn't have a light
-                }
-            }
 
 
             Vector2 pos = OsmHelpers.GetCoordinateOfOsmGeo(o).Transform(camera.GetMatrix());
@@ -207,9 +265,9 @@ namespace NauticalRenderer.SlippyMap.Layers
             sb.Draw(Icons.LightBlob, pos, null, color, MathHelper.ToRadians(135), new Vector2(Icons.LightBlob.Width / 2, Icons.LightBlob.Height), new Vector2(0.20f), SpriteEffects.None, 0f);
         }
 
-        private void DrawSectorLights(SpriteBatch sb, ICompleteOsmGeo o, Camera camera)
+        private void DrawSectorLights(SpriteBatch sb, Camera camera)
         {
-            int i = 1;
+            /*int i = 1;
             while (true)
             {
                 float startAngle;
@@ -305,6 +363,43 @@ namespace NauticalRenderer.SlippyMap.Layers
                 }
 
                 Utility.Utility.DrawArc(sb, pos, range, 5, startAngle, endAngle, color);
+            }*/
+
+            foreach (SectorLight sl in sectorLights)
+            {
+                Vector2 screenPos = sl.Coordinates.Transform(camera.GetMatrix());
+                foreach (SectorLight.Sector s in sl.Sectors)
+                {
+                    // draw dashed sector boundaries if light is sector light. If it is directional light draw dashed line in the middle
+                    if (float.IsNaN(s.Orientation))
+                    {
+                        if (s.StartAngle == s.EndAngle)
+                            continue;
+
+                        Vector2 startAnglePoint = new Vector2(
+                            screenPos.X + (float)(Math.Sin(s.StartAngle) * (s.Range + 5)),
+                            screenPos.Y - (float)(Math.Cos(s.StartAngle) * (s.Range + 5))
+                        );
+
+                        Vector2 endAnglePoint = new Vector2(
+                            screenPos.X + (float)(Math.Sin(s.EndAngle) * (s.Range + 5)),
+                            screenPos.Y - (float)(Math.Cos(s.EndAngle) * (s.Range + 5))
+                        );
+
+                        LineRenderer.DrawDashedLine(sb, new[] { screenPos, startAnglePoint }, Color.DimGray, new[] { 7f, 3f }, Matrix.Identity);
+                        LineRenderer.DrawDashedLine(sb, new[] { screenPos, endAnglePoint }, Color.DimGray, new[] { 7f, 3f }, Matrix.Identity);
+                    }
+                    else
+                    {
+                        Vector2 orientationAnglePoint = new Vector2(
+                            screenPos.X + (float)(Math.Sin(s.Orientation) * (s.Range + 5)),
+                            screenPos.Y - (float)(Math.Cos(s.Orientation) * (s.Range + 5))
+                        );
+                        LineRenderer.DrawDashedLine(sb, new[] { screenPos, orientationAnglePoint }, Color.DimGray, new[] { 7f, 3f }, Matrix.Identity);
+                    }
+
+                    Utility.Utility.DrawArc(sb, screenPos, s.Range, 5, s.StartAngle, s.EndAngle, s.Color);
+                }
             }
         }
 
